1d50cfeb2785689b7bc5a92876d8a2c71fd908a1
Huzefa-Jadliwala <huzefa.jadliwala@gmail.com>
2024-05-19 20:16:34 +0200

    Completed with the profile page UI

--- edcare-frontend/src/pages/Profile.jsx
+++ edcare-frontend/src/pages/Profile.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import { useSelector } from "react-redux";
 
 export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div>Profile</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form action="" className="flex flex-col gap-4">
+        <img
+          src={currentUser.profilePicture}
+          alt="profile"
+          className="h-24 w-24 self-center rounded-full object-cover"
+        />
+        <input
+          type="text"
+          id="username"
+          placeholder={currentUser.username}
+          className="bg-slate-100 rounded-lg p-3 mt-2"
+        />
+        <input
+          type="email"
+          id="email"
+          placeholder={currentUser.email}
+          className="bg-slate-100 rounded-lg p-3 mt-2"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="New Password"
+          className="bg-slate-100 rounded-lg p-3 mt-2"
+        />
+        <button className="uppercase bg-slate-700 p-3 rounded-lg text-white hover:opacity-95 disabled:opacity-80">
+          Update
+        </button>
+        <div className="flex justify-between ">
+          <span className="text-red-700 cursor-pointer">Delete Account</span>
+          <sapn className="text-red-700 cursor-pointer">Log Out</sapn>
+        </div>
+      </form>
+    </div>
+  );
 }
